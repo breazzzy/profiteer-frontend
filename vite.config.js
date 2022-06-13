8784539a7e2959051592c63cf35a3f29b0fd9738
breazzzy <billybarrese@comcast.net>
2022-06-13 17:12:28 -0400

    Revert "Deploy"
    This reverts commit 65b671358c113e28691cd00ace3114690ed3316c.

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,6 @@ export default defineConfig({
 });
 
 module.exports = {
-  publicPath: "/profiteer-frontend/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/profiteer-frontend/" : "/",
 };
